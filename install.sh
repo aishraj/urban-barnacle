a2e39edee1dded8a16a6cda7af4057c7c3cd4ff8
Aish <aishraj@github.com>
2022-10-28 15:18:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
-set -x
+set -xe
 
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vimrc $HOME/.vimrc
@@ -11,3 +11,13 @@ ln -s $(pwd)/emacs $HOME/.emacs
 ln -s $(pwd)/screenrc $HOME/.screenrc
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
+
+DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles
+
+# Install fish
+sudo apt-get update -y
+sudo apt-get install fish -y
+
+
+# Install Starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
