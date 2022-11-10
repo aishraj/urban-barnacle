21f61dba6dff3b69fddafad0bfc0174a7abbfb19
Aish <aishraj@users.noreply.github.com>
2022-11-10 18:12:29 +0000

    Cleanup and fixes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,10 @@
-set fish_greeting
+set -g fish_greeting ""
 set PATH /usr/local/bin $PATH
 
 alias reload "exec fish"
 
-alias git "hub"
 alias gc "git checkout"
 alias gl "git log -1 --format='%H'"
-
-# GitHub specific
-alias ghs='script/server'
-alias ghd='script/server --debug'
 
 # Setup Starship
 starship init fish | source
